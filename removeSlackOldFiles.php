e9ccc2f4e319a9c0df6092e07efe69100c715c6b
Jorge Martínez Mauricio <jorgem@gmail.com>
2016-09-11 14:44:16 -0500

    script

---
+++ removeSlackOldFiles.php
@@ -0,0 +1,31 @@
+<?php
+   /* Remove old files from slack team. * @toro 2016-09-10 */
+   setlocale(LC_ALL,'es_MX.UTF-8'); //locale settings
+   $tiempo = strtotime("-2 MONTH"); //Filter files created before this timestamp (inclusive).
+   /* get token at https://api.slack.com/web (test token it is ok!) */
+   $token= ""; //token
+   $datas = [ "token"=> $token, "ts_to"=> $tiempo]; //data sent
+   if(empty($token)) die($argv[0]." - invalid token\n");
+   /* open connection */
+   $ch = curl_init();
+   curl_setopt($ch, CURLOPT_POSTFIELDS, $datas);
+   curl_setopt($ch, CURLOPT_POST,1); curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+   curl_setopt($ch, CURLOPT_URL,"https://slack.com/api/files.list?token=".$token); 
+   $result = curl_exec($ch); $data = json_decode($result);
+   /* error */
+   if(!isset($data->ok) or empty($data->files)) { echo "No existen archivos < ".strftime("%c",$tiempo).".\n"; exit(); }
+   /* success! */
+   echo "=== Encontramos ".sizeof($data->files)." archivos más viejos de ".strftime("%c",$tiempo)."\n\n";
+   $i=0;
+   foreach($data->files AS $file) {
+      $i++;
+      echo "- Eliminando ".$i."/".sizeof($data->files)." [".date('d/m/Y',$file->created)."] ".$file->name."... ";
+      curl_setopt($ch, CURLOPT_POSTFIELDS, ['file'=>$file->id]);
+      curl_setopt($ch, CURLOPT_URL,"https://slack.com/api/files.delete?token=".$token);
+      $result = curl_exec($ch); $tmp = json_decode($result);
+      if(isset($tmp->ok)) echo "ok!\n";
+      else echo $tmp->error."\n";
+      sleep(1);
+   }
+   curl_close($ch);
+   //EOF
